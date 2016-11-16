412d507c3ba9317100bb3735db100c802c773a0c
Randy Torres <torresr14@gmail.com>
2016-11-15 18:59:26 -0800

    Finished User Auth

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -2,10 +2,11 @@ import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
   LOGIN_USER_SUCCESS,
-  LOGIN_USER_FAIL
+  LOGIN_USER_FAIL,
+  LOGIN_USER
  } from '../actions/Types';
 
-const INITIAL_STATE = { email: '', password: '', user: null };
+const INITIAL_STATE = { email: '', password: '', user: null, loading: false };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
@@ -14,9 +15,16 @@ export default (state = INITIAL_STATE, action) => {
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
     case LOGIN_USER_SUCCESS:
-      return { ...state, error: '', user: action.payload };
+      return { ...state,
+         error: '',
+         user: action.payload,
+         loading: false, 
+         password: ''
+       };
     case LOGIN_USER_FAIL:
-      return { ...state, error: 'Authentication Failed', password: '' };
+      return { ...state, error: 'Authentication Failed', password: '', loading: false };
+      case LOGIN_USER:
+      return { ...state, loading: true, error: '' };
     default:
       return state;
   }
